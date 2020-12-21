386a50a9688ec4f4f68b5afa36cf1c0a592e6f4c
Chaitali Tamboliya <chaitalitamboliya@gmail.com>
2020-12-22 01:29:22 +0530

    index.js added

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,23 @@
 const functions = require('firebase-functions');
 const express = require("express");
 const cos = require("cors");
+const stripe = require("stripe")('sk_test_51I0ZukBacOkbaJJMg6a7f4PR3T5n2TdZY4V9Iz9cf8pNiXEP3SVnqbco04WBn5xLOeZ8dL3LEImdvaugcOzcIB2N00IbavGNiI')
 
+//API
+
+//App config
+const app = express();
+
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => res.status(200).send('hello world'))
+
+// Listen command
+exports.api = functions.https.onRequest(app)
 
 
 // // Create and Deploy Your First Cloud Functions
